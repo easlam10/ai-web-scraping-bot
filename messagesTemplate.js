dc4ba25d248078a3768e4f65ae4005bd2195c63b
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-04-25 03:34:05 -0700

    code now dynamically scraps 5 websites and generates 5 whatsapp messages

---
+++ messagesTemplate.js
@@ -0,0 +1,11 @@
+const messages = [
+  {
+    uni: "NUST",
+    message:
+      `*🔔 National University of Sciences & Technology*\n` +
+      "`Entry Test`\n" +
+      `NET-IV registrations are now open.\n*Deadline:* ${deadlineDate}\n\n` +
+      "`Tap to Join, Share & Shine`\n" +
+      `https://whatsapp.com/channel/0029Vb9qWtQGE56sYuYipX1P`,
+  },
+];
