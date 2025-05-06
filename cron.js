cb0a9cab2dada71fb864d29afda913684a1f44f7
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-05-06 10:00:24 -0700

    github actions

--- cron.js
+++
@@ -1,6 +0,0 @@
-const runScrapers = require('./server');
-
-// This will be called by Render's cron job
-module.exports = async () => {
-  return await runScrapers();
-};
--- index.js
+++ index.js
@@ -16,17 +16,12 @@ async function runScrapers() {
     // await scrapGiki();
     await scrapFast();
     console.log('All scrapers completed successfully');
-    process.exit(0); // Exit successfully
   } catch (error) {
     console.error('Scraper failed:', error);
-    process.exit(1); // Exit with error
+    
   }
 }
-
-// Run immediately when invoked directly
-if (require.main === module) {
   runScrapers();
-}
 
 // Export for potential HTTP endpoint
 module.exports = runScrapers;
