a567d963f0eaeabdee75d1f6ca8a5df0cabe082c
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-07-15 18:43:31 +0500

    fixed chromium issue

--- services/fetchPageContent.js
+++ services/fetchPageContent.js
@@ -4,12 +4,16 @@ const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
 
 async function fetchPageContent(url) {
+  const browserFetcher = puppeteer.createBrowserFetcher();
+  const localRevisions = await browserFetcher.localRevisions();
+  const executablePath = (await browserFetcher.revisionInfo(localRevisions[0])).executablePath;
+  
   const browser = await puppeteer.launch({
     headless: "new",
+    executablePath,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    ignoreHTTPSErrors: true// required for cloud deployment
+    ignoreHTTPSErrors: true
   });
-
   const page = await browser.newPage();
   
   // ⏱ Increase timeout to 5 minutes (300,000 ms)
