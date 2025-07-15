c41061764c3e229895ca98f5a5bd03b7b27ae6e3
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-07-15 18:44:15 +0500

    fixed build errors

--- services/fetchPageContent.js
+++ services/fetchPageContent.js
@@ -6,16 +6,17 @@ puppeteer.use(StealthPlugin());
 async function fetchPageContent(url) {
   const browserFetcher = puppeteer.createBrowserFetcher();
   const localRevisions = await browserFetcher.localRevisions();
-  const executablePath = (await browserFetcher.revisionInfo(localRevisions[0])).executablePath;
-  
+  const executablePath = (await browserFetcher.revisionInfo(localRevisions[0]))
+    .executablePath;
+
   const browser = await puppeteer.launch({
     headless: "new",
     executablePath,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    ignoreHTTPSErrors: true
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    ignoreHTTPSErrors: true,
   });
   const page = await browser.newPage();
-  
+
   // ⏱ Increase timeout to 5 minutes (300,000 ms)
   await page.goto(url, { waitUntil: "networkidle2", timeout: 300000 });
 
