8ad9071b3190d9007832a92ed309ae839988dd81
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-07-15 18:51:48 +0500

    fixed fetchPageContent

--- services/fetchPageContent.js
+++ services/fetchPageContent.js
@@ -1,7 +1,8 @@
-const puppeteer = require("puppeteer-extra");
+const puppeteerExtra = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const puppeteer = require("puppeteer"); // for executablePath
 
-puppeteer.use(StealthPlugin());
+puppeteerExtra.use(StealthPlugin());
 
 async function fetchPageContent(url) {
   const browserFetcher = puppeteer.createBrowserFetcher();
@@ -9,15 +10,15 @@ async function fetchPageContent(url) {
   const executablePath = (await browserFetcher.revisionInfo(localRevisions[0]))
     .executablePath;
 
-  const browser = await puppeteer.launch({
+  const browser = await puppeteerExtra.launch({
     headless: "new",
     executablePath,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    ignoreHTTPSErrors: true,
+    ignoreHTTPSErrors: true, // required for cloud deployment
   });
+
   const page = await browser.newPage();
 
-  // ⏱ Increase timeout to 5 minutes (300,000 ms)
   await page.goto(url, { waitUntil: "networkidle2", timeout: 300000 });
 
   const html = await page.content();
