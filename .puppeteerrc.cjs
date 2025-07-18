96f9c287749b440c6476a0b5eb99a7c6a43f28a0
Ehtisham Aslam <ehtisham.ea10@gmail.com>
2025-07-18 22:58:50 +0500

    fixed cache issues

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,7 +1,7 @@
 const {join} = require('path');
 
 /**
- * @type {import("puppeteer").Configuration}
+ * @type {import('puppeteer').Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
